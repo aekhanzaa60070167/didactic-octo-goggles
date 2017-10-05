ef6e785e9d9560517a690377ca836976ce881bb1
aek60070167 <frong_konan@live.com>
2017-10-05 10:51:07 +0700

    เอกหล่อ แก้

--- 03.py
+++ 03.py
@@ -2,5 +2,7 @@ def cal(num1,num2):
     summ = summation(num1,num2)
     subb = subbtraction(num1,num2)
     return subb, summ
-
+def summation(n1, n2):
+	return n1 + n2
+	
 cal(int(input()), int(input()))
